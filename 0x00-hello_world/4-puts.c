abd98838fee169ac6ee495e96f273ee83ba6a3da
chaimaelaayali <chaimae.laayali@gmail.com>
2023-03-16 17:46:35 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- * main - A program that prints a line with puths fuction
- * Return: 0 (Succes)
+ * main -Print a line of code using puts
+ *
+ * Return: 0
  */
-int main(void){puts("\"Programming is like building a multilingual puzzle"); return (0);}
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
